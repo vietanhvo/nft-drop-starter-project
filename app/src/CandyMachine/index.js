4c8a9834f9491fb279c60fbc4a383f0f92cd2dc6
Anh Viet <anhviet5121pc@gmail.com>
2022-07-21 18:03:02 +0700

    Change storage to pinata

--- app/src/CandyMachine/index.js
+++ app/src/CandyMachine/index.js
@@ -13,6 +13,7 @@ import {
     getNetworkToken,
     CIVIC,
 } from "./helpers";
+import CountdownTimer from "../CountdownTimer";
 
 const { SystemProgram } = web3;
 const opts = {
@@ -398,15 +399,40 @@ const CandyMachine = ({ walletAddress }) => {
         });
     };
 
+    const renderDropTimer = () => {
+        // Get the current date and dropDate in a JavaScript Date object
+        const currentDate = new Date();
+        const dropDate = new Date(candyMachine.state.goLiveData * 1000);
+
+        // If currentDate is before dropDate, render our Countdown component
+        if (currentDate < dropDate) {
+            console.log("Before drop date!");
+            // Don't forget to pass over your dropDate!
+            return <CountdownTimer dropDate={dropDate} />;
+        }
+
+        // Else let's just return the current drop date
+        return <p>{`Drop Date: ${candyMachine.state.goLiveDateTimeString}`}</p>;
+    };
+
     return (
-        // Only show this if machineStats is available
-        candyMachine && (
+        candyMachine &&
+        candyMachine.state && (
             <div className="machine-container">
-                <p>{`Drop Date: ${candyMachine.state.goLiveDateTimeString}`}</p>
+                {renderDropTimer()}
                 <p>{`Items Minted: ${candyMachine.state.itemsRedeemed} / ${candyMachine.state.itemsAvailable}`}</p>
-                <button className="cta-button mint-button" onClick={null}>
-                    Mint NFT
-                </button>
+                {/* Check to see if these properties are equal! */}
+                {candyMachine.state.itemsRedeemed ===
+                candyMachine.state.itemsAvailable ? (
+                    <p className="sub-text">Sold Out 🙊</p>
+                ) : (
+                    <button
+                        className="cta-button mint-button"
+                        onClick={mintToken}
+                    >
+                        Mint NFT
+                    </button>
+                )}
             </div>
         )
     );
